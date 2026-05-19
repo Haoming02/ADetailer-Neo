4318aa72cac01f52afc1f0404cbf65daf280107b
Haoming <hmstudy02@gmail.com>
2026-05-19 17:26:16 +0800

    lint

--- aaaaaa/p_method.py
+++ aaaaaa/p_method.py
@@ -1,34 +1,37 @@
-from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from modules.processing import StableDiffusionProcessing
 
 
-def need_call_process(p) -> bool:
+def need_call_process(p: "StableDiffusionProcessing") -> bool:
     if p.scripts is None:
         return False
-    i = p.batch_index
-    bs = p.batch_size
+    i: int = p.batch_index
+    bs: int = p.batch_size
     return i == bs - 1
 
 
-def need_call_postprocess(p) -> bool:
+def need_call_postprocess(p: "StableDiffusionProcessing") -> bool:
     if p.scripts is None:
         return False
     return p.batch_index == 0
 
 
-def is_img2img_inpaint(p) -> bool:
-    return hasattr(p, "image_mask") and p.image_mask is not None
+def is_img2img_inpaint(p: "StableDiffusionProcessing") -> bool:
+    return getattr(p, "image_mask", None) is not None
 
 
-def is_inpaint_only_masked(p) -> bool:
-    return hasattr(p, "inpaint_full_res") and p.inpaint_full_res
+def is_inpaint_only_masked(p: "StableDiffusionProcessing") -> bool:
+    return getattr(p, "inpaint_full_res", False)
 
 
-def get_i(p) -> int:
-    it = p.iteration
-    bs = p.batch_size
-    i = p.batch_index
+def get_i(p: "StableDiffusionProcessing") -> int:
+    it: int = p.iteration
+    bs: int = p.batch_size
+    i: int = p.batch_index
     return it * bs + i
 
 
-def is_skip_img2img(p) -> bool:
+def is_skip_img2img(p: "StableDiffusionProcessing") -> bool:
     return getattr(p, "_ad_skip_img2img", False)
