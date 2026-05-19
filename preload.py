683d20c8b0ad960a37037a171a890bacb6dcd3a0
Haoming <hmstudy02@gmail.com>
2026-05-19 16:49:34 +0800

    deps

--- preload.py
+++ preload.py
@@ -5,5 +5,5 @@ def preload(parser: argparse.ArgumentParser):
     parser.add_argument(
         "--ad-no-huggingface",
         action="store_true",
-        help="Don't use adetailer models from huggingface",
+        help="Do not automatically download YOLO models",
     )
